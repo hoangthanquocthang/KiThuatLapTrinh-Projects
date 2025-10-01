8825210f9c8dbcd357ad01a3c1bd2f05bb9a0ef7
Hoàng Thân Quốc Thắng <qthang1610@gmail.com>
2025-10-02 01:53:49 +0700

    Update bai 11 ktlt.cpp

--- bai 11 ktlt.cpp
+++ bai 11 ktlt.cpp
@@ -1,31 +1,21 @@
-#include <stdio.h>
-
-int main() {
-    int soBoTest;
-    printf("Nhap so bo test: ");
-    scanf("%d", &soBoTest);
-    while (soBoTest--) {
-        int giaSanPham, soTienTra;
-        scanf("%d %d", &giaSanPham, &soTienTra);
-        int soTienThua = soTienTra - giaSanPham;
-        int soTo10Dollar, soTo5Dollar, soTo1Dollar;
-        soTo10Dollar = soTienThua / 10;
-        int soTienConLai = soTienThua % 10;
-        if (soTienConLai >= 5) {
-            soTo5Dollar = 1;
-            soTienConLai -= 5;
-        } else {
-            soTo5Dollar = 0;
-        }
-        soTo1Dollar = soTienConLai;
-        printf("%d = %d * 10 + %d * 5 + %d * 1\n", 
-               soTienTra - giaSanPham, 
-               soTo10Dollar, 
-               soTo5Dollar, 
-               soTo1Dollar1,
-			   );
-    }
-
-    return 0;
+#include<stdio.h>
+int main()
+{
+	int gia,tien,tienthua,to10=0,to5=0,to1=0;
+	printf("nhap gia cua san pham ");scanf("%d",&gia);
+	printf("nhap so tien khach tra ");scanf("%d",&tien);
+	tienthua=tien-gia;
+	to10=tienthua/10;
+	tienthua=tienthua%10;
+	printf("\nso to 10 do phai dung la %d",to10);
+	printf("\ntienthua %d",tienthua);
+	to5=tienthua/5;
+	tienthua=tienthua%5;
+	printf("\nso to 5 do phai dung la %d",to5);
+	printf("\ntienthua %d",tienthua);
+	to1=tienthua/1;
+	tienthua=tienthua%1;
+	printf("\nso to 1 do phai dung la %d",to1);
+	printf("\ntienthua %d",tienthua);
 }
 
